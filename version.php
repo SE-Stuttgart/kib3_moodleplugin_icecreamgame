868c53483ca887af23486d2a5803891f04077d70
Dirk Väth <dirk.vaeth@ims.uni-stuttgart.de>
2022-08-30 17:24:25 +0200

    removed grade feature, bumped version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_icecreamgame';
-$plugin->release = '0.1.0';
-$plugin->version = 20220720010;
+$plugin->release = '1.0.0';
+$plugin->version = 20220720011;
 $plugin->requires = 2020061514;
 $plugin->maturity = MATURITY_STABLE;
